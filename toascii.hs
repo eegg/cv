e49d26d4792fcd6e71e32d204f38e74451bf42c8
James Fisher <jameshfisher@gmail.com>
2011-07-23 15:01:33 +0100

    a Data.Map is better

--- toascii.hs
+++ toascii.hs
@@ -1,12 +1,14 @@
 -- GH Pages stupidly doesn't serve plaintext as utf-8.
 -- This converts some unicode characters to roughly equivalent ASCII.
 
+import qualified Data.Map as Map
 import Data.String.Utils
 
-main = interact $ join "" . map c
+main = interact $ join "" . map (\c -> case (Map.lookup c m) of { Nothing -> [c]; Just s -> s })
 
-c '’' = "'"
-c '–' = "-"
-c '…' = "..."
-c '‑' = "-" -- non-breaking hyphen
-c a   = [a]
+m = Map.fromList [
+  ('’', "'"),
+  ('–', "-"),
+  ('…', "..."),
+  ('‑', "-") -- non-breaking hyphen
+  ]
