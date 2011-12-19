fddcef5ed56ebc706b22aa130347bf4ba9464e86
James Fisher <jameshfisher@gmail.com>
2011-12-19 16:12:16 +0000

    update for latest round of apps

--- toascii.hs
+++ toascii.hs
@@ -2,9 +2,8 @@
 -- This converts some unicode characters to roughly equivalent ASCII.
 
 import qualified Data.Map as Map
-import Data.String.Utils
 
-main = interact $ join "" . map (\c -> case (Map.lookup c m) of { Nothing -> [c]; Just s -> s })
+main = interact $ concat . map (\c -> case (Map.lookup c m) of { Nothing -> [c]; Just s -> s })
 
 m = Map.fromList [
   ('’', "'"),
